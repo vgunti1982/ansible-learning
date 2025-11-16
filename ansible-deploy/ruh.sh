deb264e67e34718760b8199a43eb0556482070aa
vgunti1982 <139302472+vgunti1982@users.noreply.github.com>
2025-11-15 19:45:31 -0600

    Comment out ansible commands in ruh.sh
    Comment out ansible commands in ruh.sh

--- ansible-deploy/ruh.sh
+++ ansible-deploy/ruh.sh
@@ -1,1 +1,3 @@
-ANSIBLE_HOST_KEY_CHECKING=False ansible all -i hosts.ini -m ping
+# ANSIBLE_HOST_KEY_CHECKING=False ansible all -i hosts.ini -m ping
+# ansible-playbook -i hosts.ini configure-env.yml
+# ansible all -i hosts.ini -m shell -a "id deploy && sudo -l -U deploy" --become
