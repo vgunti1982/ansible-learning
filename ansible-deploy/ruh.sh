9cb2f72e1a05f6f2d83e7b4ef9d3b7d267539a32
vgunti1982 <139302472+vgunti1982@users.noreply.github.com>
2025-11-15 19:48:53 -0600

    Add command to check myapp status and health

--- ansible-deploy/ruh.sh
+++ ansible-deploy/ruh.sh
@@ -1,3 +1,4 @@
 # ANSIBLE_HOST_KEY_CHECKING=False ansible all -i hosts.ini -m ping
 # ansible-playbook -i hosts.ini configure-env.yml
 # ansible all -i hosts.ini -m shell -a "id deploy && sudo -l -U deploy" --become
+# ansible web -i hosts.ini -m shell -a "systemctl is-active myapp && curl -s http://localhost:9090 | head -n1" --become
